3679e94f1ad36086e912846e60d32fdf72fe4298
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-05-26 18:39:30 +0100

    Add logic to show the first quiz question when start quiz button is clicked

--- Assets/js/questions.js
+++ Assets/js/questions.js
@@ -34,12 +34,21 @@ var questions = [
   },
 ];
 
-// When start quiz button is clicked, timer must start
+// When "start quiz" button is clicked:
+// 1. start timer
+// 2. show first question
+
+const quizIntro = document.querySelector("#quiz-intro-container");
 const startButton = document.querySelector("#start-btn");
 const timer = document.querySelector(".timer");
 
-startButton.addEventListener("click", () => {
-  let countdown = 61;
+const questionContainer = document.querySelector("#question-container");
+const quizQuestion = document.querySelector("#question");
+const answerButtons = document.querySelectorAll(".answer-btn");
+const buttonsContainer = document.querySelector("#answers");
+
+startButton.addEventListener("click", (event) => {
+  let countdown = 60;
 
   const reduceTimer = setInterval(() => {
     if (countdown > 0) {
@@ -49,4 +58,50 @@ startButton.addEventListener("click", () => {
       clearInterval(reduceTimer);
     }
   }, 1000);
+
+  quizIntro.textContent = "";
+
+  quizQuestion.textContent = `${questions[0].title}`;
+  quizQuestion.setAttribute("style", "text-align: center");
+
+  answerButtons.forEach((button, i) => {
+    button.textContent = `(${i + 1}). ${questions[0].choices[i]}`;
+  });
 });
+
+// correctAnswer = questions[0].answer;
+
+// if (event.target === correctAnswer) {
+//   alert("CORRECT!");
+// }
+
+// element = event.target;
+
+// if (element.matches("#start-btn")) {
+//   state = quizQuestion.getAttribute("data-state");
+//   console.log(state, "<-------");
+// }
+
+// quizQuestion.textContent = `Q1: ${questions[0].title}`;
+// questions[0].choices.forEach((choice) => {
+//   answers.textContent = choice;
+// });
+
+// ------------------ FEEDBACK -------------------
+// button.addEventListener("click", (event) => {
+//   if (button.textContent === questions[0].answer) {
+//     const horizontalLine = document.createElement("hr");
+//     const correctResponse = document.createElement("p");
+//     correctResponse.textContent = "Correct ✅";
+
+//     questionContainer.appendChild(horizontalLine);
+//     questionContainer.appendChild(correctResponse);
+//   } else {
+//     const horizontalLine = document.createElement("hr");
+//     const wrongResponse = document.createElement("p");
+//     wrongResponse.textContent = "Incorrect ❌";
+
+//     questionContainer.appendChild(horizontalLine);
+//     questionContainer.appendChild(wrongResponse);
+//   }
+// });
