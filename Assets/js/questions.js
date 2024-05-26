cefa81be66aa8119946bfc4474f12e7753145b85
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-05-26 14:48:37 +0100

    Start timer when start quiz button is clicked

--- Assets/js/questions.js
+++ Assets/js/questions.js
@@ -1,35 +1,52 @@
 // list of all questions, choices, and answers
 var questions = [
   {
-    title: 'Commonly used data types DO NOT include:',
-    choices: ['strings', 'booleans', 'alerts', 'numbers'],
-    answer: 'alerts',
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts",
   },
   {
-    title: 'The condition in an if / else statement is enclosed within ____.',
-    choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
-    answer: 'parentheses',
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses",
   },
   {
-    title: 'Arrays in JavaScript can be used to store ____.',
+    title: "Arrays in JavaScript can be used to store ____.",
     choices: [
-      'numbers and strings',
-      'other arrays',
-      'booleans',
-      'all of the above',
+      "numbers and strings",
+      "other arrays",
+      "booleans",
+      "all of the above",
     ],
-    answer: 'all of the above',
+    answer: "all of the above",
   },
   {
     title:
-      'String values must be enclosed within ____ when being assigned to variables.',
-    choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
-    answer: 'quotes',
+      "String values must be enclosed within ____ when being assigned to variables.",
+    choices: ["commas", "curly brackets", "quotes", "parentheses"],
+    answer: "quotes",
   },
   {
     title:
-      'A very useful tool used during development and debugging for printing content to the debugger is:',
-    choices: ['JavaScript', 'terminal / bash', 'for loops', 'console.log'],
-    answer: 'console.log',
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+    answer: "console.log",
   },
 ];
+
+// When start quiz button is clicked, timer must start
+const startButton = document.querySelector("#start-btn");
+const timer = document.querySelector(".timer");
+
+startButton.addEventListener("click", () => {
+  let countdown = 61;
+
+  const reduceTimer = setInterval(() => {
+    if (countdown > 0) {
+      countdown--;
+      timer.textContent = `Time remaining ⏳: ${countdown} s`;
+    } else {
+      clearInterval(reduceTimer);
+    }
+  }, 1000);
+});
